2b16b47b2e01f6948891a7cd05075035e20d5a21
utkarsh139 <mishrautkarsh281@gmail.com>
2026-03-06 15:40:33 +0530

    feat: add bcrypt password hashing

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,3 +1,4 @@
+const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 const User = require("../models/User");
 
@@ -11,19 +12,23 @@ exports.registerUser = async (req, res) => {
       return res.status(400).json({ message: "User already exists" });
     }
 
+    //hashing password
+    const hashedPassword = await bcrypt.hash(password, 10)
     // create user
     const user = await User.create({
       name,
       email,
-      password,
+      password: hashedPassword,
     });
+
+
 
     res.status(201).json({
       message: "User registered successfully ✅",
       user,
     });
   } catch (error) {
-    res.status(500).json({ message: "Server error ❌", error });
+    res.status(500).json({ message: "Server error ❌", error: error.message });
   }
 };
 
@@ -32,7 +37,13 @@ exports.loginUser = async (req, res) => {
     const { email, password } = req.body;
 
     const user = await User.findOne({ email });
-    if (!user || user.password !== password) {
+    // check if user exists using bcrypt to compare password
+    if (!user) {
+      return res.status(400).json({ message: "Invalid email or password" });
+    }
+
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+    if (!isPasswordValid) {
       return res.status(400).json({ message: "Invalid email or password" });
     }
 
@@ -43,23 +54,23 @@ exports.loginUser = async (req, res) => {
       { expiresIn: "1h" }
     );
 
-        const safeUser = {
-        id: user._id,
-        name: user.name,
-        email: user.email,
-        role: user.role || "user",
-        twilio: {
-            phoneNumber: user.twilio?.phoneNumber,
-            isConnected: user.twilio?.isConnected,
-        },
-        };
+    const safeUser = {
+      id: user._id,
+      name: user.name,
+      email: user.email,
+      role: user.role || "user",
+      twilio: {
+        phoneNumber: user.twilio?.phoneNumber,
+        isConnected: user.twilio?.isConnected,
+      },
+    };
 
-        res.status(200).json({
-        message: "Login successful ✅",
-        token,
-        user: safeUser,
-        });
+    res.status(200).json({
+      message: "Login successful ✅",
+      token,
+      user: safeUser,
+    });
   } catch (error) {
-    res.status(500).json({ message: "Server error ❌", error });
+    res.status(500).json({ message: "Server error ❌", error: error.message });
   }
 };
