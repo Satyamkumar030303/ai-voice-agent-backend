0646440fe2cdcbe641285acffabd173ea030b78c
Ansh Mani Tripathi <anshmanitripathi171203@gmail.com>
2026-03-09 13:15:59 +0530

    Added a seprate UI for manage Agents

--- src/middleware/authMiddleware.js
+++ src/middleware/authMiddleware.js
@@ -4,8 +4,6 @@ exports.protect = (req, res, next) => {
   try {
     const authHeader = req.headers.authorization;
 
-    console.log("Auth Header:", authHeader);
-
     // Check if token exists
     if (!authHeader || !authHeader.startsWith("Bearer ")) {
       return res.status(401).json({ message: "Not authorized ❌" });
@@ -16,8 +14,6 @@ exports.protect = (req, res, next) => {
     // Verify token
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
 
-    console.log("DECODED:", decoded);
-
     // Attach user to request
     req.user = {
       _id: decoded.userId,
@@ -25,8 +21,6 @@ exports.protect = (req, res, next) => {
 
     next(); // 🔥 continue to route
   } catch (error) {
-    console.log("JWT ERROR:", error.message);
-
     if (error.name === "TokenExpiredError") {
       return res.status(401).json({
         message: "Token expired, please login again",
